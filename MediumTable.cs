4b775ae7d74568a8ccb1bf2e7b86f45704c47333
Maximilian Walz <peantz@gmx.de>
2023-09-15 09:37:29 +0200

    Windows + classes created

---
+++ MediumTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWP_WPF_Project
+{
+    internal class MediumTable
+    {
+        public string mediumtitle;
+        public double rating;
+        public DateTime watched;
+        public int season;
+        public bool movie;
+        public bool show;
+    }
+}
